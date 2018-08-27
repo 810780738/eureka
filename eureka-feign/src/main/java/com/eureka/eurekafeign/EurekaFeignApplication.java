3bc8d164783686b7595aec4022dc29c687dc3403
zhusm <810780738@qq.com>
2018-08-27 18:21:42 +0800

    添加熔断器监听器

--- eureka-feign/src/main/java/com/eureka/eurekafeign/EurekaFeignApplication.java
+++ eureka-feign/src/main/java/com/eureka/eurekafeign/EurekaFeignApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableHystrixDashboard  //启用熔断器监听器
 public class EurekaFeignApplication {
 
 	public static void main(String[] args) {
