1865fe266fc26ae3d277c25adddd427e85249bdf
zhusm <810780738@qq.com>
2018-08-27 17:45:58 +0800

    添加 熔断器监听器

--- eureka-ribbon/src/main/java/com/eureka/eurekaribbon/EurekaRibbonApplication.java
+++ eureka-ribbon/src/main/java/com/eureka/eurekaribbon/EurekaRibbonApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableHystrix
+@EnableEurekaClient //启用Eureka客户端
+@EnableHystrix  //启用熔断器
+@EnableHystrixDashboard  //启用熔断器监听器
 public class EurekaRibbonApplication {
 
 	public static void main(String[] args) {
